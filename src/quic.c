ee8fd78a41886ff2e6fa48f41f5bca6fadedade3
adikabintang <adika.sulaeman@aalto.fi>
2019-07-11 17:19:54 +0300

    pass test variable length integer decoding

--- src/quic.c
+++ src/quic.c
@@ -2,9 +2,48 @@
 
 uint64_t decode_var_len_int(u_char *header_field)
 {
-    int var_len;
+    uint8_t var_len = 0;
+    uint64_t value = 0;
+    uint8_t usable_bit = 0;
+
     // assuming big endian
     var_len = (*header_field & 0b11000000) >> 6;
-    printf("%x", var_len);
-    return 0;
+    switch (var_len)
+    {
+    case 0b00:
+        usable_bit = 6;
+        break;
+    case 0b01:
+        usable_bit = 14;
+        break;
+    case 0b10:
+        usable_bit = 30;
+        break;
+    case 0b11:
+        usable_bit = 62;
+        break;
+    default:
+        break;
+    }
+
+    // printf("usable bit: %d\n", usable_bit);
+
+    u_char *hdr_pointer = header_field;
+    value = *hdr_pointer & 0b00111111;
+    usable_bit -= 6;
+    // printf("v: %x\n", value);
+
+    while (usable_bit > 0) {
+        // printf("usable bit: %d\n", usable_bit);
+        hdr_pointer += 1;
+        value = (value << 8) & 0xFFFFFFFFFFFFFFFF;
+        // printf("v> %x\n", value);
+        // printf("h: %x\n", *hdr_pointer);
+        value = value | *hdr_pointer;
+        // printf("v? %x\n", value);
+        usable_bit = usable_bit - 8;
+    }
+    
+    // printf("%x\n", var_len);
+    return value;
 }
--- test/quic_test.c
+++ test/quic_test.c
@@ -3,13 +3,28 @@ TODO: this should be a unit test with cmocka. not like this.
  */
 
 #include <stdio.h>
+#include <inttypes.h>
 #include "quic.h"
 
 int main()
 {
+    uint64_t x;
     // test cases: https://tools.ietf.org/html/draft-ietf-quic-transport-20#section-16
-    //u_char header_field[] = {0x7b, 0xbd};
-    u_char header_field[] = {0xc2, 0x19, 0x7c, 0x5e, 0xff, 0x14, 0xe8, 0x8c};
-    uint64_t x = decode_var_len_int(header_field);
+    u_char header_field[] = {0x25};
+    x = decode_var_len_int(header_field);
+    printf("%" PRIu64 "\n", x);
+    
+
+    u_char header_field_1[] = {0x7b, 0xbd};
+    x = decode_var_len_int(header_field_1);
+    printf("%" PRIu64 "\n", x);
+
+    u_char header_field_2[] = {0x9d, 0x7f, 0x3e, 0x7d};
+    x = decode_var_len_int(header_field_2);
+    printf("%" PRIu64 "\n", x);
+
+    u_char header_field_3[] = {0xc2, 0x19, 0x7c, 0x5e, 0xff, 0x14, 0xe8, 0x8c};
+    x = decode_var_len_int(header_field_3);
+    printf("%" PRIu64 "\n", x);
     return 0;
 }
